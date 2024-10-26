49c20362f00518b32674b502fae8560af4a10bd4
Karthick <karthickkp2005@gmail.com>
2024-10-26 11:54:39 +0530

    Synchronised ros communication, Added wire.begintransmission in setup since sensor is set to continuous mode

--- main.cpp
+++ main.cpp
@@ -3,57 +3,58 @@
 #include <Wire.h>
 #include<Servo.h>
 #include "std_msgs/Int64.h"
+#include "std_msgs/Bool.h"
 #include "geometry_msgs/Vector3.h"
 
 const int slaveadd=0x1E;
-int dt=100;
+int dt=50;
 int outpin=3;
 int x,value;
 Servo myservo;
 float mfx,mfy,mfz;
+long i=0;
+
+geometry_msgs::Vector3 magnetic_field;
+std_msgs::Int64 pwmval;
 
 ros::NodeHandle nh;
-geometry_msgs::Vector3 magnetic_field;
-//std_msgs::Int64 pwmval;
-
+void bldc(const std_msgs::Int64& pwmval); 
 ros::Publisher chatter_pub1("mf",&magnetic_field);
-
-int bldc(const std_msgs::Int64&pwm);
-
-ros::Subscriber <std_msgs::Int64> sub("yaw",&bldc);
-
+ros::Subscriber<std_msgs::Int64> sub("pwm", &bldc);
 void setup() 
 {
-  //Serial.begin(57600);
-  // delay(dt);
-  // while(!Serial)
-  // {
-  //   delay(1);
-  // }
-
-  // Serial.println("hello");
-
+  Serial.begin(9600);
+  while(!Serial)
+  {
+    delay(1);
+  }
+  
   pinMode(outpin,OUTPUT);
   myservo.attach(outpin);
   myservo.writeMicroseconds(1478);
   delay(100);
-
-
+  Wire.setSDA(PB7);
+  Wire.setSCL(PB6);
   Wire.begin();
   Wire.beginTransmission(slaveadd);
   Wire.write(0x00);
-  Wire.write(0x70);
+  Wire.write(0x78);
   Wire.endTransmission(true);
 
   Wire.beginTransmission(slaveadd);
   Wire.write(0x01);
-  Wire.write(0xA0);
+  Wire.write(0xA0);  
   Wire.endTransmission(true);
 
   Wire.beginTransmission(slaveadd);
   Wire.write(0x02);
-  Wire.write(0x00);
+  Wire.write(0xFC);   
   Wire.endTransmission(true);
+
+  Wire.beginTransmission(slaveadd);
+  Wire.write(0x03);
+  Wire.endTransmission(false);
+
 
   nh.initNode();
   nh.advertise(chatter_pub1);
@@ -63,11 +64,8 @@ void setup()
 
 void loop() 
 {
-  Wire.beginTransmission(slaveadd);
-  Wire.write(0x03);
-  Wire.endTransmission(false);
-  Wire.requestFrom(slaveadd,6,true);
 
+  Wire.requestFrom(slaveadd,6,true);
   if(Wire.available())//if we have 6 bytes to be read
   {
     magnetic_field.x=(Wire.read()<<8|Wire.read()); //msb value is shifted left
@@ -76,29 +74,30 @@ void loop()
   }
   else
   {
-    //Serial.println("data not available");
+    Serial.println("data not available");
   }
 
   chatter_pub1.publish(&magnetic_field);
 
-  // Serial.print("x:");
-  // Serial.println(magnetic_field.x);
-  // Serial.print("y:");
-  // Serial.println(magnetic_field.y);
-  // Serial.print("z:");
-  // Serial.println(magnetic_field.z);
-
-  nh.spinOnce();
+  Serial.print("x:");
+  Serial.println(magnetic_field.x);
+  Serial.print("y:");
+  Serial.println(magnetic_field.y);
+  Serial.print("z:");
+  Serial.println(magnetic_field.z);
   delay(dt);
+  i++;
+  if(i==1000)
+  i=0;
+  nh.spinOnce();
 
 }
 
-int bldc(const std_msgs::Int64& pwmval)
+void bldc(const std_msgs::Int64& pwmval)
 {
   value=pwmval.data;
-  //value=1700;
   x=map(value,1000,2000,1100,1900);//0 corresponds to full speed in one dirn, 180 for opp, 90 for stop
-  
-  //Serial.println(x);
   myservo.writeMicroseconds(x);  
 }
+
+
